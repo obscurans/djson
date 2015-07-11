d6637c4409a2638ea7e991159be8b4400faa85f4
obscurans <jeffrey.tsang@ieee.org>
2015-07-11 05:24:08 -0400

    Implement numeric/array/nested object parsing/printing
    Add null wrapper type as boolean enum for void "type" properties
    Extend parsing/printing to integral/real/array/nested object types
    Moved parsing/printing templates outside JSONobjectImpl class
    String parsing now handles escape sequences except \uXXXX
    Unittest for parsing/printing added

--- src/json.d
+++ src/json.d
@@ -1,6 +1,6 @@
 /** Copyright (C) 2015 Jeffrey Tsang. All rights reserved. See /LICENCE.md */
 
-import std.string, std.format, std.conv, std.traits, std.math;
+import std.string, std.format, std.conv, std.math, std.traits, std.exception;
 
 alias JSONobject(Specification...) = JSONobjectImpl!(validateJSONspec!Specification);
 
@@ -10,6 +10,8 @@ struct JSONattributes {
 
 immutable JSONattributes Default = JSONattributes.init;
 immutable JSONattributes Required = JSONattributes(true);
+
+enum JSONnull : bool { BLANK, GIVEN };
 
 private:
 /* Basic sanitization to turn a string into a valid D identifier. Simply drops
@@ -21,10 +23,12 @@ pure string sanitize(string name) {
 }
 
 class ParseException : Exception {
-	this(string message) {
+	pure this(string message) {
 		super(message);
 	}
 }
+
+enum bool isJSONobject(T) = (hasMember!(T, "JSONparser") && is(typeof({T x; string y; x.JSONparser(y);}())));
 
 /* Standard dummy template that simply wraps its template arguments as a
  * palpable type, for chaining template argument manipulations */
@@ -54,6 +58,12 @@ if (name.length == 1) {
 	alias validateJSONspec = validateJSONspec!([name[0], name[0]], Tail);
 }
 
+/* Changes a void type into a special null wrapper type */
+template validateJSONspec(string[] name, Type : void, Tail...)
+if (name.length == 2) {
+	alias validateJSONspec = validateJSONspec!(name, JSONnull, Tail);
+}
+
 /* Using template specialization rules, detects omission of the JSONattributes
  * parameter, and inserts the default */
 template validateJSONspec(string[] name, Type, Tail...)
@@ -71,7 +81,7 @@ if (name.length == 2) {
 /* Extra templates to cover the field of invalid specifications, to help with
  * error messages. */
 template validateJSONspec(Orphan, Tail...) {
-	mixin JSONspecError!(Orphan, Tail); /* Template to construct error message string */
+	mixin JSONspecError!(Orphan, Tail); // Template to construct error message string
 	/* Raise a compiler error */
 	static assert(0, "Invalid JSON specification (property name(s) expected) at: " ~ specErrorString);
 }
@@ -114,6 +124,7 @@ version(unittest) {
 	enum string createSpecGroup(string prefix, int num) = "alias SpecGroup" ~ prefix ~ "= Tuple!(" ~ SpecGroupNames!("Spec" ~ prefix, num) ~ ");";
 }
 
+/* Static unittest: JSON specification validation/expansion */
 unittest {
 	/* Testing methodology: storing Tuples as compile-time static arrays of
 	 * either JSONobject specifications, or groups of specifications */
@@ -197,17 +208,79 @@ unittest {
 	}
 }
 
+unittest {
+	alias JSONobject!(
+		"field", void)
+		TestNull;
+
+	alias JSONobject!(
+		"single", bool,
+		"array", bool[])
+		TestBoolean;
+
+	alias JSONobject!(
+		"single", int,
+		"array", int[])
+		TestIntegral;
+
+	alias JSONobject!(
+		"single", double,
+		"array", double[])
+		TestReal;
+
+	alias JSONobject!(
+		"single", string,
+		"array", string[])
+		TestString;
+
+	alias JSONobject!(
+		"Array", TestBoolean[],
+		"String", TestString,
+		"Real", TestReal,
+		"Integral", TestIntegral,
+		"Boolean", TestBoolean,
+		"Null", TestNull)
+		TestNested;
+
+	/* Comprehensive test of parsing/printing capabilities */
+	TestNested x = TestNested(` { "Null" : { "field" : null } ,
+		"Boolean": {"single": true, "array": [true, false, true]},
+		"Integral" :{"single" :-1234 ,"array" :[ -1 ,23 ,4 ]} ,
+		"Real" : { "single" : 0.123e-4, "array" : [ 0.1, 2, 3e-4 ] },
+		"String":{"single":"test","array":["foo","bar","baz"]},
+		"Array" : [ {}, { "single":true } ]
+		} `);
+
+	assert(x.Boolean.single == true);
+	assert(x.Boolean.array == [true, false, true]);
+	assert(x.Integral.single == -1234);
+	assert(x.Integral.array == [-1, 23, 4]);
+	assert(x.Real.single == 0.123e-4);
+	assert(x.Real.array == [0.1, 2, 3e-4]);
+	assert(x.String.single == "test");
+	assert(x.String.array == ["foo", "bar", "baz"]);
+	assert(x.Array[1].single == true);
+
+	/* TODO: properly predict and test printing capabilities in toto
+	 * Issues: floating-point output */
+	assert(x.Null.toString() == `{"field":null}`);
+	assert(x.Boolean.toString() == `{"single":true,"array":[true,false,true]}`);
+	assert(x.Integral.toString() == `{"single":-1234,"array":[-1,23,4]}`);
+	assert(x.String.toString() == `{"single":"test","array":["foo","bar","baz"]}`);
+
+	assertThrown!ParseException(TestNested(`{`));
+}
+
 /* Completely generic templated struct (with the names and types of properties
  * given as parameters) which automagically generates members/parser/printer
- * functionality.
- */
+ * functionality. */
 struct JSONobjectImpl(Specification...) {
 
 	static assert(is(this == JSONobjectImpl!(validateJSONspec!Specification)), "Invalid JSON specification");
 
 	mixin JSONvar_decl!Specification;
 
-	pure string toString() const {
+	string toString() const {
 		return "{" ~ JSONprinter!Specification() ~ "}";
 	}
 
@@ -217,8 +290,7 @@ struct JSONobjectImpl(Specification...) {
 
 private:
 	/* Creates valid D code that declares all the variables listed in the JSON
-	 * specification.
-	 */
+	 * specification. */
 	mixin template JSONvar_decl() {}
 
 	mixin template JSONvar_decl(string[] name, Type, JSONattributes attr, Tail...) {
@@ -236,19 +308,11 @@ private:
 		string ret = "";
 
 		void printvar() {
-			ret = `"` ~ name[0] ~ `":`;
-			static if (isSomeString!Type) {
-				ret ~= `"`;
-			}
-			ret ~= to!string(mixin(name[1]));
-			static if (isSomeString!Type) {
-				ret ~= `"`;
-			}
+			ret = `"` ~ name[0] ~ `":` ~ JSONprintvar!Type(mixin(name[1]));
 		}
 
 		/* Expression tests if null can be implicitly converted to Type, in
-		 * which case an explicit null value can be stored for field "name"
-		 */
+		 * which case an explicit null value can be stored for field "name" */
 		static if (!attr.required && is(typeof(null) : Type)) {
 			if (mixin(name[1]) !is null) {
 				printvar();
@@ -272,54 +336,26 @@ private:
 			"skipWhitespace();" ~
 			"matchCharacter(':');" ~
 			"skipWhitespace();" ~
-			`mixin JSONreadvar!("` ~ name[1] ~ `",` ~ Type.stringof ~ ") _read_" ~ name[1] ~ ";" ~
-			"_read_" ~ name[1] ~ ".JSONreadvar();" ~
+			`mixin JSONreadvar!(` ~ Type.stringof ~ ") _read_" ~ name[1] ~ ";" ~
+			"_read_" ~ name[1] ~ ".JSONreadvar(" ~ name[1] ~ ");" ~
 			"break;" ~
 			JSONvar_parsecases!Tail;
 
-	/* Various cases for parsing properties, by type. Currently only strings are
-	 * implemented */
-	void JSONreadvar(string name, Type)() {
-		static assert(0, "Field parsing of type " ~ Type.stringof ~ " currently not implemented");
-	}
-
-	void JSONreadvar(string name, Type : void)() {
-		if (input[0 .. 4] == "null") {
-			input = input[4 .. $];
-		} else {
-			throw new ParseException(format(`Invalid input, "null" expected in: %s`, input));
-		}
-	}
-
-	void JSONreadvar(string name, Type : bool)() {
-		if (input[0 .. 4] == "true") {
-			mixin(name) = true;
-			input = input[4 .. $];
-		} else if (input[0 .. 5] == "false") {
-			mixin(name) = false;
-			input = input[5 .. $];
-		} else {
-			throw new ParseException(format(`Invalid input, "true" or "false" expected in: %s`, input));
-		}
-	}
-
-	void JSONreadvar(string name, Type : long)() {
-//		static assert(0, "Not implemented");
-	}
-
-	void JSONreadvar(string name, Type : real)() {
-//		static assert(0, "Not implemented");
-	}
-
-	void JSONreadvar(string name, Type : string)() {
-		mixin(name) = parseString();
-	}
-
 	/* Recursive templates to validate the read-in JSON object. Currently just
 	 * checks whether non-optional properties that could be null are not null */
-	void JSONvar_validate()() {}
+	pure void JSONvar_validate()() {}
 
-	void JSONvar_validate(string[] name, Type, JSONattributes attr, Tail...)() {
+	pure void JSONvar_validate(string[] name, Type : JSONnull, JSONattributes attr, Tail...)() {
+		static if (attr.required) {
+			if (mixin(name[1]) == JSONnull.BLANK) {
+				throw new ParseException("Mandatory field " ~ name[0] ~ " not given");
+			}
+		}
+
+		JSONvar_validate!Tail();
+	}
+
+	pure void JSONvar_validate(string[] name, Type, JSONattributes attr, Tail...)() {
 		static if (attr.required && is(typeof(null) : Type)) {
 			if (mixin(name[1]) is null) {
 				throw new ParseException("Mandatory field " ~ name[0] ~ " not given");
@@ -330,22 +366,51 @@ private:
 	}
 
 	/* Compile-time construction of the parser, which takes an input string and
-	 * fills in the properties of the JSON object */
-	void JSONparser(string input) {
+	 * fills in the properties of the JSON object. Destructively modifies the
+	 * input string, stripping off the part read. */
+	pure void JSONparser(ref string input) {
 		void skipWhitespace() {
 			munch(input, " \t\r\n");
 		}
 
+		bool testCharacter(char c) {
+			if (!input.length) {
+				throw new ParseException(format("Invalid input, input exhausted"));
+			} else {
+				return input[0] == c;
+			}
+		}
+
 		void matchCharacter(char c) {
-			if (input[0] == c) {
+			if (input.length && input[0] == c) {
 				input = input[1 .. $];
 			} else {
 				throw new ParseException(format("Invalid input, %c expected in: %s", c, input));
 			}
 		}
 
+		void matchOptionalSeparator() {
+			skipWhitespace();
+			static if (false) {
+				/* Extension of ECMA404 JSON: accepts extraenous ',' property
+				 * separators and ignores them */
+				// Could be replaced with munch(input, whitespace ~ ",")
+				while (munch(input, ",").length) {
+					skipWhitespace();
+				}
+			}
+		}
+
+		void matchSeparator() {
+			skipWhitespace();
+			matchCharacter(',');
+			matchOptionalSeparator();
+		}
+
 		string parseString() {
-			ptrdiff_t index = 0;
+			ptrdiff_t lastindex = 0, index = 0;
+			string ret = "";
+
 			skipWhitespace();
 			matchCharacter('"');
 			do {
@@ -355,17 +420,35 @@ private:
 				}
 
 				if (input[index] == '"') {
-					break; /* Unescaped closing " */
+					break; // Unescaped closing "
 				} else {
-					index += 2; /* Blindly skip next character after \ */
+					ret ~= input[lastindex .. index]; // Copy up to escape sequence
+					if (index + 1 >= input.length) {
+						throw new ParseException("Invalid input, unterminated escape sequence in string");
+					}
+
+					switch (input[index + 1]) {
+					case '"': case '\\': case '/': ret ~= input[index + 1]; break;
+					case 'b': ret ~= '\b'; break;
+					case 'f': ret ~= '\f'; break;
+					case 'n': ret ~= '\n'; break;
+					case 'r': ret ~= '\r'; break;
+					case 't': ret ~= '\t'; break;
+					case 'u': /*TODO*/ break;
+					default:
+						static if (false) {
+							/* Extension of ECMA404 JSON: accepts invalid escapes,
+							 * treated as literal character ignoring backslash */
+							ret ~= input[index + 1];
+						} else {
+							throw new ParseException(format("Invalid input, invalid escape sequence at: %s", input[index .. $]));
+						}
+					}
+					lastindex = index + 2;
 				}
 			} while (1);
 
-			if (index > input.length) {
-				throw new ParseException("Invalid input, unterminated escape sequence in string");
-			}
-
-			string ret = input[0 .. index];
+			ret ~= input[lastindex .. index]; // Copy end of string
 			input = input[index .. $];
 			matchCharacter('"');
 
@@ -375,27 +458,182 @@ private:
 		/* Begin body of parse(string) */
 		skipWhitespace();
 		matchCharacter('{');
+		matchOptionalSeparator();
 
-		do {
-			/* Extension of RFC7159 JSON specs, accepts extraenous ','
-			 * property separators and ignores them */
-			/* Could be replaced with munch(input, whitespace ~ ",") */
-			do {
-				skipWhitespace();
-			} while (munch(input, ",").length);
-			if (input[0] == '}') {
-				break;
-			}
-
+		while (!testCharacter('}')) {
 			string name = parseString();
 			switch (name) {
 				mixin(JSONvar_parsecases!Specification);
 				default: throw new ParseException("Invalid field name: " ~ name);
 			}
-		} while (1);
+
+			skipWhitespace();
+			if (testCharacter('}')) {
+				break;
+			}
+			matchSeparator();
+		}
+		input = input[1 .. $];
 
 		mixin JSONvar_validate!Specification;
 		JSONvar_validate();
 	}
+}
+
+/* Various template functions used to print variables to string
+ *
+ * Base case, directly use toString(), which also handles nested JSONobjects
+ */
+string JSONprintvar(Type)(const Type var) {
+	return to!string(var);
+}
+
+/* Null wrapper type should be printed as "null" token */
+pure string JSONprintvar(Type : JSONnull)(const Type var) {
+	return "null";
+}
+
+/* String types need quotation and escaping */
+string JSONprintvar(Type : string)(const Type var) {
+	/* TODO: re-escape the string */
+	return `"` ~ to!string(var) ~ `"`;
+}
+
+/* Array types need [] wrapping and , separators, printing each element */
+string JSONprintvar(Type : Type[])(const Type[] var) {
+	string ret = "[";
+
+	if (var.length) {
+		ret ~= JSONprintvar!Type(var[0]);
+		foreach (x; var[1 .. $]) {
+			ret ~= "," ~ JSONprintvar!Type(x);
+		}
+	}
+
+	return ret ~ "]";
+}
+
+/* Various cases for parsing properties, by type.
+ *
+ * These functions are invoked by mixin within JSONparser
+ */
+void JSONreadvar(Type)(ref Type var)
+if (!isJSONobject!Type) {
+	static assert(0, "Field parsing of type " ~ Type.stringof ~ " currently not implemented");
+}
+
+/* A null property requires the exact token "null" */
+void JSONreadvar(Type : JSONnull)(ref Type var) {
+	if (input[0 .. 4] == "null") {
+		var = JSONnull.GIVEN;
+		input = input[4 .. $];
+	} else {
+		throw new ParseException(format(`Invalid input, "null" expected in: %s`, input));
+	}
+}
+
+/* A boolean property requires one of the exact tokens "true" or "false" */
+void JSONreadvar(Type : bool)(ref Type var) {
+	if (input[0 .. 4] == "true") {
+		var = true;
+		input = input[4 .. $];
+	} else if (input[0 .. 5] == "false") {
+		var = false;
+		input = input[5 .. $];
+	} else {
+		throw new ParseException(format(`Invalid input, "true" or "false" expected in: %s`, input));
+	}
+}
+
+/* Simple hand-written state machine to match ECMA404 number tokens, returning
+ * the prefix matched without semantic interpretation. Destructive read on the
+ * input string to match other uses. */
+static pure string parseNumber(ref string input) {
+	size_t length = 0;
+
+	if (length < input.length && input[length] == '-') { // -?
+		length++;
+	}
+
+	if (length >= input.length) { // 0|[1-9][0-9]*
+	} else if (input[length] == '0') {
+		length++;
+	} else if (input[length] >= '1' && input[length] <= '9') {
+		do {
+			length++;
+		} while (length < input.length && (input[length] >= '0' && input[length] <= '9'));
+	} else {
+		throw new ParseException(format("Invalid input, invalid number token at: %s", input));
+	}
+
+	if (length < input.length && input[length] == '.') { // (\.[0-9]*)?
+		do {
+			length++;
+		} while (length < input.length && (input[length] >= '0' && input[length] <= '9'));
+	}
+
+	if (length < input.length && (input[length] == 'e' || input[length] == 'E')) { // ([eE][+-]?[0-9]*)?
+		length++;
+		if (length < input.length && (input[length] == '+' || input[length] == '-')) {
+			length++;
+		}
+
+		while (length < input.length && (input[length] >= '0' && input[length] <= '9')) {
+			length++;
+		}
+	}
+
+	string ret = input[0 .. length];
+	input = input[length .. $];
+	return ret;
+}
+
+/* Integral numeric properties are parsed then interpreted by standard library,
+ * which agrees semantically with ECMA404 */
+void JSONreadvar(Type : long)(ref Type var) {
+	var = to!Type(parseNumber(input));
+}
+
+/* Real numeric properties are parsed then interpreted by standard library,
+ * which agrees semantically with ECMA404 */
+void JSONreadvar(Type : real)(ref Type var) {
+	var = to!Type(parseNumber(input));
+	static if (false) {
+		/* Eventual hook to handle special float values as strings */
+	}
+}
+
+/* String parsing is already present in JSONparser */
+void JSONreadvar(Type : string)(ref Type var) {
+	var = parseString();
+}
+
+/* Handles statically-typed JSON arrays */
+void JSONreadvar(Type : Type[])(ref Type[] var) {
+	skipWhitespace();
+	matchCharacter('[');
+	matchOptionalSeparator();
+
+	while (!testCharacter(']')) {
+		var.length++;
+
+		mixin .JSONreadvar!(Type) element;
+		element.JSONreadvar(var[$ - 1]);
+
+		skipWhitespace();
+		if (testCharacter(']')) {
+			break;
+		}
+		matchSeparator();
+	}
+	input = input[1 .. $];
+}
+
+/* If Type is itself a JSONobject (duck typing only), read it by chaining onto
+ * the parser for the inner object. Requires parser to destructively read from
+ * input. */
+void JSONreadvar(Type)(ref Type var)
+if (isJSONobject!Type) {
+	var.JSONparser(input);
 }
 
