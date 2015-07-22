d3e72fe11597e877a3b2bc42d6ddce179a1f070a
obscurans <jeffrey.tsang@ieee.org>
2015-07-22 11:44:05 -0400

    Implement static array-based version of UnionFind

--- dutil/unionfind.d
+++ dutil/unionfind.d
@@ -80,3 +80,72 @@ class UnionFind(T) {
 	}
 }
 
+/* Static array-based version of UnionFind. Has no payloads, only contiguous
+ * indices stored. Index of a set is immutable on creation.
+ *
+ * this(size_t): initialize with this number of singleton sets
+ * makeSet(): append one singleton set
+ * makeSet(size_t): append this number of singleton sets
+ * @property length: number of sets
+ * find(size_t): find root of set with this index
+ * unionSet(size_t, size_t): join two sets with these indices, returns new root
+ */
+struct UnionFindStatic {
+	this(size_t count) {
+		set_info.length = count;
+		set_info[] = -1; /* Set all to singleton sets */
+	}
+
+	void makeSet() {
+		set_info ~= -1;
+	}
+
+	void makeSet(size_t count) {
+		set_info.length += count;
+		set_info[$ - count .. $] = -1; /* Set to singleton sets */
+	}
+
+	@property pure size_t length() const {
+		return length;
+	}
+
+	size_t find(size_t index) {
+		if (set_info[index] >= 0) {
+			set_info[index] = find(set_info[index]); /* Path compression */
+		}
+		return index;
+	}
+
+	size_t unionSet(size_t first, size_t second) {
+		size_t first_root = find(first);
+		size_t second_root = find(second);
+
+		if (first_root == second_root) {
+			return first_root;
+		}
+
+		ptrdiff_t first_rank = set_info[first_root]; /* Two's complemented */
+		ptrdiff_t second_rank = set_info[second_root];
+		assert(first_rank < 0);
+		assert(second_rank < 0);
+
+		/* Union by rank: attach lower rank to higher ranked root */
+		if (first_rank > second_rank) { /* Reverse test under complementation */
+			set_info[first_root] = second_root;
+			return second_root;
+		} else if (first_rank < second_rank) {
+			set_info[second_root] = first_root;
+			return first_root;
+		} else {
+			set_info[second_root] = first_root; /* Arbitrary choice at equal rank */
+			set_info[first_root]-- /* Increase rank by 1 */
+			return first_root;
+		}
+	}
+
+private:
+	/* Array storage for each set. If positive, points to the index of its
+	 * parent. If negative, is the root of a tree, rank is two's complemented */
+	ptrdiff_t[] set_info;
+}
+
